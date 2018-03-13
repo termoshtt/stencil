0536f13762be9f87e7cf900febb7e50ef9bf0aca
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-03-13 18:40:31 +0900

    Fill documents

--- src/padding.rs
+++ src/padding.rs
@@ -1,17 +1,25 @@
+//! Typed Padding
+
+/// Trait for typed paddings
 pub trait Padding: Clone + Copy {
+    /// Length of padding
     fn len() -> usize;
 }
 
-pub trait LessEq<P: Padding>: Padding {}
-pub trait GreaterEq<P: Padding>: Padding {}
-pub trait EqualTo<P: Padding>: Padding {}
-
+/// padding one element
 #[derive(Clone, Debug, Copy)]
 pub struct P1 {}
+
+/// padding two element
 #[derive(Clone, Debug, Copy)]
 pub struct P2 {}
+
+/// padding three element
 #[derive(Clone, Debug, Copy)]
 pub struct P3 {}
+
+/// Compare padding sizes
+pub trait LessEq<P: Padding>: Padding {}
 
 impl LessEq<P1> for P1 {}
 impl LessEq<P2> for P1 {}
@@ -20,12 +28,18 @@ impl LessEq<P3> for P1 {}
 impl LessEq<P3> for P2 {}
 impl LessEq<P3> for P3 {}
 
+/// Compare padding sizes
+pub trait GreaterEq<P: Padding>: Padding {}
+
 impl GreaterEq<P1> for P1 {}
 impl GreaterEq<P1> for P2 {}
 impl GreaterEq<P1> for P3 {}
 impl GreaterEq<P2> for P2 {}
 impl GreaterEq<P2> for P3 {}
 impl GreaterEq<P3> for P3 {}
+
+/// Compare padding sizes
+pub trait EqualTo<P: Padding>: Padding {}
 
 impl<Pa: LessEq<Pb> + GreaterEq<Pb>, Pb: Padding> EqualTo<Pb> for Pa {}
 

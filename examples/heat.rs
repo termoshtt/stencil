25f60a4226add0f6a9b2fc0deb13f2a8ef3ce577
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-03-17 15:10:51 +0900

    Add fixed boundary condition for diffusion equation

--- examples/heat.rs
+++ examples/heat.rs
@@ -2,10 +2,14 @@ extern crate asink;
 extern crate ndarray;
 extern crate stencil;
 
-use stencil::*;
 use ndarray::*;
 use std::sync::mpsc::Sender;
+use std::f64::consts::PI;
 use asink::Sink;
+
+use stencil::*;
+use stencil::region::*;
+use stencil::padding::P1;
 
 fn periodic(r: Sender<Vec<f64>>) {
     let dt = 1e-3;
@@ -13,7 +17,23 @@ fn periodic(r: Sender<Vec<f64>>) {
     t.coordinate_fill(|x| x.sin());
     let dx = t.dx();
     let mut s = t.clone();
-    for _step in 0..100_000 {
+    for _step in 0..3000 {
+        t.stencil_map(&mut s, |n: N1D1<f64>| {
+            let d2 = (n.l + n.r - 2.0 * n.c) / (dx.powi(2));
+            n.c + dt * d2
+        });
+        r.send(s.as_view().to_vec()).unwrap();
+        ::std::mem::swap(&mut t, &mut s);
+    }
+}
+
+fn fixed(r: Sender<Vec<f64>>) {
+    let dt = 1e-3;
+    let mut t = Line::<f64, P1, Closed>::new(64, 0.0, 1.0, 2.5 * PI);
+    t.coordinate_fill(|x| x.sin());
+    let dx = t.dx();
+    let mut s = t.clone();
+    for _step in 0..10000 {
         t.stencil_map(&mut s, |n: N1D1<f64>| {
             let d2 = (n.l + n.r - 2.0 * n.c) / (dx.powi(2));
             n.c + dt * d2
@@ -24,8 +44,13 @@ fn periodic(r: Sender<Vec<f64>>) {
 }
 
 fn main() {
-    let sink = asink::json::JsonSink::from_str("heat.json");
+    let sink = asink::json::JsonSink::from_str("heat_periodic.json");
     let (r, th) = sink.run();
     periodic(r);
+    th.join().unwrap();
+
+    let sink = asink::json::JsonSink::from_str("heat_fixed.json");
+    let (r, th) = sink.run();
+    fixed(r);
     th.join().unwrap();
 }
